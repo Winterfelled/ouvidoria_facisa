1b9ed14b3a9ebc71bf20d4297b7ed21f56e0801a
Pedro Nunes <sudwesterlied154@gmail.com>
2025-06-04 15:38:52 -0300

    Atualizado com opções placeholder.

--- menu_sql.py
+++ menu_sql.py
@@ -1,10 +1,36 @@
 from operacoesbd import criarConexao
 
+
 conexao = criarConexao(endereco="127.0.0.1",usuario="root",senha="programa@11",bancodedados="ouvidoria")
 
 opcao = -1
 
-while opcao != 6:
+while opcao != 7:
 
     print("\n- Bem vindo ao sistema de ouvidoria da unifacisa -")
-    print("1) Listar manifestações 2) Coloque seu feedback 3)Remover feedback ")
+    print("1) Listar manifestações | 2) Listar Manifestações por código | 3) Criar uma nova manisfestação")
+    print("4) Exibir quantidade de manifestações | 5) Pesquisar manifestação por código 6) Excluir uma manifestação por código")
+    print("7) Sair do sistema")
+
+    opcao = int(input("Digite a opção desejada: "))
+
+    if opcao == 1:
+        print("listar")
+
+    elif opcao == 2:
+        print("listar por codigo")
+
+    elif opcao == 3:
+        print("criar nova manisfestacao")
+
+    elif opcao == 4:
+        print("Exibir quantidade de manifestações")
+
+    elif opcao == 5:
+        print("Pesquisar manifestação por código")
+
+    elif opcao == 6:
+        print("Excluir por código")
+
+    elif opcao != 7:
+        print("Opcão inválida.")
